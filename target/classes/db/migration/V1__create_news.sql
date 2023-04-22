e3873dd3e35a4c7bd3d63f165824dbe33306f14c
iNfidelAim <110556919+iNfidelAim@users.noreply.github.com>
2023-04-22 11:29:57 +0700

    Delete target directory

--- target/classes/db/migration/V1__create_news.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS news;
-CREATE TABLE news (
-    id int8 NOT NULL,
-    published BOOLEAN,
-    source VARCHAR(255),
-    title VARCHAR(255),
-    topic VARCHAR(255),
-    PRIMARY KEY (id));
--- target/classes/db/migration/V2__create_person.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS person;
-CREATE TABLE person (
-    id  SERIAL NOT NULL,
-    password VARCHAR(255),
-    role VARCHAR(255),
-    username VARCHAR(255),
-    PRIMARY KEY (id));
